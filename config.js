e930ca026bb8e3e8f0b611d7476def3e8592ff3f
Sergiu <106928950+sergiubt@users.noreply.github.com>
2025-12-14 13:15:23 +0100

    Update lavalink server password

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ module.exports = {
 			identifier: "Main Node", //- Used for indentifier in stats commands.
 			host: "", //- The host name or IP of the lavalink server.
 			port: 80, // The port that lavalink is listening to. This must be a number!
-			password: "", //- The password of the lavalink server.
+			password: "youshallnotpass", //- The password of the lavalink server.
 			retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
 			retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
 			secure: false, //- Can be either true or false. Only use true if ssl is enabled!
